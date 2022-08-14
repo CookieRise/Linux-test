ac311da6809a07ebf0f4045f4101d9345f907312
CookieRise <1098923861@qq.com>
2022-08-14 10:25:17 +0800

    修改提交

--- testcommit.c
+++ testcommit.c
@@ -1,9 +1,9 @@
-#include<iostream.h>
 #include<stdio.h>
 
 int main()
 {
-	printf("hellow world.\n");
+	printf("update testcommit.c this new output.\n");
+	printf("hello world.\n");
 	return 0;
 }
 
